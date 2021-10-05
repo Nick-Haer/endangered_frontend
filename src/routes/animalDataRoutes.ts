06aca52efda0cc95df8106387a1c746e936393f6
Nick.Haer <nhaer123@gmail.com>
2021-10-05 01:01:50 -0500

    logging for heroku build

--- src/routes/animalDataRoutes.ts
+++ src/routes/animalDataRoutes.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const HOST = process.env.HOST
+console.log(HOST, 'hosty');
 export const getAllanimals = () => axios.get(`${HOST}/species/getAllAnimals`)
 
 export const getMatchingCharities = (name: string) => axios.get(`${HOST}/species/${name}/getMatchingCharities`)

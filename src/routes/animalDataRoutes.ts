bb1888dab3efae0089c036bb1b0f8b0fe5f50345
Nick.Haer <nhaer123@gmail.com>
2021-10-05 01:12:22 -0500

    correct address

--- src/routes/animalDataRoutes.ts
+++ src/routes/animalDataRoutes.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const HOST = 'https://boiling-mesa-93887.herokuapp.com/'
+const HOST = 'https://boiling-mesa-93887.herokuapp.com'
 export const getAllanimals = () => axios.get(`${HOST}/species/getAllAnimals`)
 
 export const getMatchingCharities = (name: string) => axios.get(`${HOST}/species/${name}/getMatchingCharities`)

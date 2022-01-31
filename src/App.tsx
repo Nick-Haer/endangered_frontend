3d8e1f6c94c1908c510e38d4829e6bac1a86516a
Nick.Haer <nhaer123@gmail.com>
2022-01-30 20:02:15 -0600

    remove remaining warnings

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import AnimalCardsContainer from './components/Animal/AnimalCardsContainer';
 
--- src/components/Animal/AnimalOverviewCard.tsx
+++ src/components/Animal/AnimalOverviewCard.tsx
@@ -1,6 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Box, Typography, Grid, Paper } from "@material-ui/core";
-import { getAllanimals } from "../../routes/animalDataRoutes";
+import React from "react";
+import { Typography, Paper } from "@material-ui/core";
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from "clsx";
 
